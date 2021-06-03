d0e5f6ad3da564e19b6047ef01a6d0565049c83a
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-06-03 17:02:18 -0300

    subindo o banco

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,9 +1,17 @@
 const express =  require('express');
-
+const mongoose = require('mongoose')
 const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/rastro-dev',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+    )
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
 app.listen(3000, () => {console.log("servidor na porta 3000 iniciado")});
 
 app.get('/', 
@@ -11,7 +19,6 @@ app.get('/',
     console.log("Tudo certo na pasta raiz");
     response.send("Olá");
 })
-
 app.post('/rastreador', (request, response) => {
     console.log("Rota rastreador chamada com sucesso");
     console.log(request.body)
