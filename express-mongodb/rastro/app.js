dd84fbe45dd59170fcb3c3c16ed2791b5d72c5e3
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-06-03 16:09:02 -0300

    correção no codigo

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,10 +1,8 @@
-const { response } = require('express');
 const express =  require('express');
 
 const app = express();
-
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.listen(3000, () => {console.log("servidor na porta 3000 iniciado")});
 
