372a5eaf3c7ae5811c1bdd9155693fe871816a72
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-06-03 16:06:40 -0300

    preparando api no postman

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,1 +1,21 @@
-const express =  require('express')
+const { response } = require('express');
+const express =  require('express');
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.listen(3000, () => {console.log("servidor na porta 3000 iniciado")});
+
+app.get('/', 
+(request, response) => {
+    console.log("Tudo certo na pasta raiz");
+    response.send("Olá");
+})
+
+app.post('/rastreador', (request, response) => {
+    console.log("Rota rastreador chamada com sucesso");
+    console.log(request.body)
+    response.send("ok")
+})
